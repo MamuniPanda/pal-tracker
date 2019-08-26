14369580822568b90efc985839bd5314536a32ff
PAL-1 <Mamuni_Panda@dell.com>
2019-08-26 15:14:08 +0530

    pipeline

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace PalTracker
 {
     [Route("/")]
-    public class WelcomeController: ControllerBase
-    {       
-        [HttpGet]        
-            public string SayHello() => "Hello";       
+    public class WelcomeController : ControllerBase
+    {
+        private readonly WelcomeMessage _message;
 
+        [HttpGet]
+        public string SayHello() => _message.Message;
+
+        public WelcomeController(WelcomeMessage message)
+        {
+            _message = message;
+        }
     }
 }
-
-
-
